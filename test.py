9a9c7f11b17ecb18394dfb949dba13457077f64a
Maximellerbach <38923282+Maximellerbach@users.noreply.github.com>
2018-12-30 11:02:56 +0100

    added files

---
+++ test.py
@@ -0,0 +1,57 @@
+import keras
+from keras.models import load_model
+import cv2
+from tqdm import tqdm
+import numpy as np
+import h5py
+from glob import glob
+import time
+
+#LOAD AI
+model = load_model('C:\\Users\\maxim\\Desktop\\autre IA\\AI.h5')
+
+#CREATING AN ARRAY CONTAINING INPUT IMAGE'S PATH
+dos = glob('C:\\Users\\maxim\\Desktop\\autre IA\\image\\*.jpg')
+
+#INITIALIZE THE SIZE OF THE CROPPED IMAGE
+wi = 28
+he = 28
+
+#SETTINGS FOR THE PUTTEXT FONCTION 
+font = cv2.FONT_HERSHEY_SIMPLEX
+fontColor = (255,0,0)
+lineType = 2
+
+
+for img_path in tqdm(dos):
+
+    img = cv2.imread(img_path,1)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    th = cv2.adaptiveThreshold(img_gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV,31,24)
+
+    im2, cnts, hierarchy = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for cont in tqdm(cnts):
+        try:
+            x, y, w, h = cv2.boundingRect(cont)
+            if h*w>20:
+                img2= th[y-4:y+h+4, x-4:x+w+4]
+                img2 = cv2.resize(img2,(wi,he))
+
+                img_pred = np.reshape(img2,(1,28,28,1))
+                
+                predicted = np.argmax(model.predict(img_pred))
+                cv2.imwrite('C:\\Users\\maxim\\Desktop\\autre IA\\cropped_image\\'+str(predicted)+'_'+str(time.time())+'.png',img2)
+                if predicted == 11:
+                    fontScale = 1
+                    cv2.putText(img, 'M', (x,y+h), font, fontScale, fontColor, lineType)
+                elif predicted != 10:
+                    fontScale = 1
+                    cv2.putText(img, str(predicted), (x,y+h), font, fontScale, fontColor, lineType)
+        except:
+            pass
+            
+    cv2.imwrite('C:\\Users\\maxim\\Desktop\\autre IA\\input_image\\'+(img_path.split('\\')[-1]).split('.')[0]+'_'+'.png',img)
+
+    
---
+++ train.py
@@ -0,0 +1,58 @@
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras import backend as K
+
+from glob import glob
+import cv2
+import numpy as np
+
+batch_size = 128
+num_classes = 12
+epochs = 16
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+x_train=[]
+y_train=[]
+
+dos = glob('C:\\Users\\maxim\\Desktop\\autre IA\\label\\*')
+for img_path in dos:
+    img = cv2.imread(img_path,0)
+    img = cv2.resize(img,(img_rows,img_cols))
+    img = np.reshape(img,(img_rows,img_cols,1))
+    x_train.append(img)
+    y_train.append((img_path.split('\\')[-1]).split('_')[0])
+
+input_shape = (img_rows, img_cols, 1)
+
+x_train = np.array(x_train).astype('float32')
+
+x_train /= 255
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+
+model = Sequential()
+
+model.add(Conv2D(256, (4, 4), activation='relu', input_shape=input_shape))
+model.add(Dense(256, activation='relu'))
+MaxPooling2D(pool_size=(8, 8), padding='same')
+
+model.add(Conv2D(256, (2, 2), activation='relu', input_shape=input_shape))
+model.add(Dense(256, activation='relu'))
+MaxPooling2D(pool_size=(4, 4), padding='same')
+
+model.add(Dropout(0.1))
+
+model.add(Flatten())
+model.add(Dense(256, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy, optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=1, validation_data=(x_train, y_train))
+
+model.save('C:\\Users\\maxim\\Desktop\\autre IA\\AI.h5')
